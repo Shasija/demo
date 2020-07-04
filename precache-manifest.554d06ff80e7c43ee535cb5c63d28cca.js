d5b07aefd489d719cb4b5a70ce80e9172fb8eeca
sankalp.hubilo <sankalp.hubilo@gmail.com>
2020-07-04 12:48:08 +0530

    Updates

---
+++ precache-manifest.554d06ff80e7c43ee535cb5c63d28cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfa838c0f1ffe17af9c2c3ccb8c46f7",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "ff27ba17df8ea8df67e3",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "faf47e8a0afa599a0917",
+    "url": "/demo/static/js/2.29938b7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.29938b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff27ba17df8ea8df67e3",
+    "url": "/demo/static/js/main.6e963a6f.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
